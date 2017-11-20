aabb507754591f2cf92fcc3b6366354eff2d6043
Praveen <praveen@GiGroup.com>
2017-11-20 11:02:27 +0530

    name changes

--- www/ionic_prvn_location.js
+++ www/ionic_prvn_location.js
@@ -4,6 +4,7 @@ function prvn_location(){
 }
 
 prvn_location.prototype.getLatAndLong= function (successCallback, errorCallback) {
+  console.log("test callig main method")
   cordova.exec(successCallback, errorCallback, "ionic_prvn_location", "get_location_values", []);
 };
 
@@ -16,3 +17,4 @@ prvn_location.install = function () {
   window.plugins.prvn_location = new prvn_location();
   return window.plugins.prvn_location;
 };
+cordova.addConstructor(prvn_location.install);
